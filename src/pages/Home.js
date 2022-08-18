c64167ae08200a16a770bb921d9c298402a60067
kwan-hee <yrats1234@gmail.com>
2022-08-18 23:24:33 +0900

    spinning 구현

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,18 +3,40 @@ import { MovieAction } from '../redux/actions/MovieAction';
 import { useDispatch, useSelector } from 'react-redux'; 
 import { useSearchParams } from 'react-router-dom';
 import Banner from '../components/Banner';
+import MovieSlide from '../components/MovieSlide';
+import ClipLoader from "react-spinners/ClipLoader";
+
+
 
 function Home() {
     const dispatch = useDispatch()
-    const {popularMovies, topRatedMoives, upComingMovies} = useSelector(state=>state.movie)
-
+    const {popularMovies, topRatedMoives, upComingMovies, loading} = useSelector(state=>state.movie)
+    console.log("loading: ",loading)
    
+    
+    
   useEffect(()=>{
     dispatch(MovieAction.getMoives())
   },[])
+  // loading 이 true 이면 loading 스피너를 보여주고
+  // loading 이 false 이면 데이터를 보여준다. 
+  // true: 데이터 도착전
+  // false: 데이터 도착후, 에러일때
+
+    if(loading){
+      return <ClipLoader color="#000000" loading={loading} size={150} />
+    }
     return (
     <div>
-    {popularMovies.results && <Banner movie={popularMovies.results[0]}/>} 
+     
+     <Banner movie={popularMovies.results[4]}/>
+
+      <h1>popular Movie</h1>
+      <MovieSlide movies={popularMovies}/>
+      <h1>Top rated Movie</h1>
+      <MovieSlide movies={topRatedMoives}/>
+      <h1>Upcoming Movie</h1>
+      <MovieSlide movies={upComingMovies}/>
     </div>
   )
 }
